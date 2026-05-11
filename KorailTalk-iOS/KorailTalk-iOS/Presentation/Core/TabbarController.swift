3bccbf1000ef4f9d4a1167de791feac2c2facd56
jiinjang <jiinjang0@gmail.com>
2026-05-12 00:08:09 +0900

    [feat] #17 - Tabbar 버튼 배치

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/Core/TabbarController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/Core/TabbarController.swift
@@ -4,5 +4,80 @@
 //
 //  Created by mandoo on 5/10/26.
 //
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+final class TabbarController: UITabBarController {
+    
+    //MARK: - UI Components
+    
+    private let customBar = BaseUIView().then{
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 10
+        $0.layer.maskedCorners = [
+            .layerMinXMinYCorner,
+            .layerMaxXMinYCorner
+        ]
+    }
+    
+    private let homeTab = TabbarButton(
+        normalImage: .icHome,
+        selectedImage: .icHomeActive,
+        title: "홈"
+    )
+    private let benefitTab = TabbarButton(
+        normalImage: .icBenefit,
+        selectedImage: .icBenefitActive,
+        title: "혜택·정기권"
+    )
+    private let travelTab = TabbarButton(
+        normalImage: .icTravelpass,
+        selectedImage: .icTravelpassActive,
+        title: "상품·패스"
+    )
+    private let ticketTab = TabbarButton(
+        normalImage: .icMyticket,
+        selectedImage: .icMyticketActive,
+        title: "나의 티켓"
+    )
+    
+    
+    private lazy var customTabBar = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.alignment = .center
+        $0.spacing = 10
+        $0.addArrangedSubviews(homeTab,benefitTab,travelTab,ticketTab)
+    }
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.isHidden = true
+        
+        view.addSubview(customBar)
+        
+        customBar.addSubview(customTabBar)
+        
+        setLayout()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setLayout() {
+        customBar.snp.makeConstraints {
+            $0.bottom.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(91)
+        }
+        
+        customTabBar.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(18)
+        }
+    }
+}
