a39cb6ee9c671871c622202340693952802d9a84
ser0kim <srkim0728@gmail.com>
2026-05-11 15:47:30 +0900

    [feat] #11 - 네비게이션바 Test

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/TicketConfirmation/ViewController/TicketConfirmationViewController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/TicketConfirmation/ViewController/TicketConfirmationViewController.swift
@@ -5,4 +5,48 @@
 //  Created by mandoo on 5/10/26.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class TicketConfirmationViewController: BaseUIViewController {
+    
+    // MARK: - UI
+    
+    private let navigationBar = TopNavigationBar()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setUI()
+        setLayout()
+        setNavigationBar()
+    }
+    
+    // MARK: - Public Methods
+
+    
+    override func setUI() {
+        view.addSubview(navigationBar)
+    }
+    
+    override func setLayout() {
+        navigationBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(110)
+        }
+    }
+    
+    // MARK: - Custom Methods
+    
+    func setNavigationBar() {
+        navigationBar.configure(
+            title: "좌석 조회",
+            showsRefreshButton: true
+        )
+    }
+}
