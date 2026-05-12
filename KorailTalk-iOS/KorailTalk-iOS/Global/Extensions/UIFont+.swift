80548a77d15c8b22a730961daafadd3b0f973868
sumchive <haon0319@naver.com>
2026-05-12 21:59:02 +0900

    [chore] #16 - 누락된 font 추가

--- KorailTalk-iOS/KorailTalk-iOS/Global/Extensions/UIFont+.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Global/Extensions/UIFont+.swift
@@ -14,28 +14,28 @@ extension UIFont {
     
     enum Pretendard {
         case header1, header2, header3
-        case body1, body2, body3
+        case body1, body2, body3, body4
         case caption1, caption2, caption3
         
         var weight: String {
             switch self {
             case .header1, .header2, .header3:
                 "Pretendard-SemiBold"
-            case .body1, .body2, .caption1:
+            case .body1, .body2, .body3, .caption1:
                 "Pretendard-Medium"
-            case .body3, .caption2, .caption3:
+            case .body4, .caption2, .caption3:
                 "Pretendard-Regular"
             }
         }
         
         var size: CGFloat {
             switch self {
-            case .header1 : 24
-            case .header2 : 20
-            case .header3, .body1 : 16
-            case .body2, .body3 : 14
+            case .header1: 24
+            case .header2, .body1: 20
+            case .header3, .body2: 16
+            case .body3, .body4: 14
             case .caption1, .caption2: 12
-            case .caption3 : 10
+            case .caption3: 10
             }
         }
         
