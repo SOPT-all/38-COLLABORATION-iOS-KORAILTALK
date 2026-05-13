28edfcd7491288eb30a448bcb00798e2f1ac3dd9
sumchive <haon0319@naver.com>
2026-05-14 01:59:27 +0900

    [chore] #25 - BottomSheet aciton 코드 세팅

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/SeatSelection/ViewController/SeatSelectionViewController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/SeatSelection/ViewController/SeatSelectionViewController.swift
@@ -5,4 +5,43 @@
 //  Created by mandoo on 5/10/26.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class SeatSelectionViewController: BaseUIViewController {
+    
+    // MARK: - Property
+    
+    private var paymentBottomView: PaymentBottomSheetView?
+    
+    // MARK: - Action
+    
+    @objc
+    private func showPaymentBottomSheet() {
+        guard paymentBottomView == nil else { return }
+        
+        let paymentbottomSheetView = PaymentBottomSheetView()
+        
+        view.addSubview(paymentbottomSheetView)
+        
+        paymentbottomSheetView.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.height.equalTo(150)
+        }
+        
+        paymentbottomSheetView.show()
+        paymentBottomView = paymentbottomSheetView
+    }
+    
+    @objc
+    private func hidePaymentBottomSheet() {
+        guard let paymentbottomSheetView = paymentBottomView else { return }
+        
+        paymentbottomSheetView.hide { [weak self] in
+            paymentbottomSheetView.removeFromSuperview()
+            self?.paymentBottomView = nil
+        }
+    }
+}
