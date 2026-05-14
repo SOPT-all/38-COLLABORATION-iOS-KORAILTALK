b0b509a4cdd06864d52a090b347a8aaf16cfdcec
jiinjang <jiinjang0@gmail.com>
2026-05-14 21:11:12 +0900

    [del] #26 - 테스트 코드 제거

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/TrainSearch/ViewController/TrainSearchViewController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/TrainSearch/ViewController/TrainSearchViewController.swift
@@ -10,41 +10,17 @@ import UIKit
 import SnapKit
 import Then
 
-final class TrainSearchViewController: BaseUIViewController{
-    private let tableView = UITableView(frame: .zero,style: .plain)
-    private let standardButton = FareOptionButton(type: .standard)
-    private let specialButton = FareOptionButton(type: .special)
-    private let soldoutButton = FareOptionButton(type: .soldout)
-    override func viewDidLoad(){
+final class TrainSearchViewController: BaseUIViewController {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
         setLayout()
     }
     
-    override func setUI(){
-        //self.view.addSubview(tableView)
-        self.view.addSubviews(standardButton,specialButton,soldoutButton)
+    override func setUI() {
     }
     
-    override func setLayout(){
-        /*tableView.snp.makeConstraints{
-            $0.edges.equalToSuperview()
-        }*/
-        standardButton.snp.makeConstraints{
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(50)
-            $0.width.height.equalTo(100)
-        }
-        specialButton.snp.makeConstraints{
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(standardButton.snp.bottom).offset(50)
-            $0.width.height.equalTo(100)
-        }
-        soldoutButton.snp.makeConstraints{
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(specialButton.snp.bottom).offset(50)
-            $0.width.height.equalTo(100)
-        }
+    override func setLayout() {
     }
 }
