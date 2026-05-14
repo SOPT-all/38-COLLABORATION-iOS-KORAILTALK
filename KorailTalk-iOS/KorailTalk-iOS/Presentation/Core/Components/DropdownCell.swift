2b14d7b6075176214edf3e387d8b8ad3fdac966c
ser0kim <srkim0728@gmail.com>
2026-05-14 22:51:12 +0900

    [feat] #19 - 드롭다운 cell 구현

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/Core/Components/DropdownCell.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/Core/Components/DropdownCell.swift
@@ -5,3 +5,69 @@
 //  Created by 김세령 on 5/12/26.
 //
 
+import UIKit
+
+import SnapKit
+import Then
+
+final class DropdownCell: UITableViewCell {
+    
+    static let identifier = "DropdownCell"
+    
+    // MARK: - UI Components
+
+    private let titleLabel = UILabel()
+    
+    // MARK: - Initializer
+
+    override init(
+        style: UITableViewCell.CellStyle,
+        reuseIdentifier: String?
+    ) {
+        super.init(
+            style: style,
+            reuseIdentifier: reuseIdentifier
+        )
+
+        setStyle()
+        setUI()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("error")
+    }
+    
+    // MARK: - Custom Methods
+
+    private func setStyle() {
+        backgroundColor = .white
+        selectionStyle = .none
+
+        titleLabel.do {
+            $0.font = .pretendard(.caption1)
+            $0.textColor = .neutral700
+        }
+    }
+
+    private func setUI() {
+        contentView.addSubview(titleLabel)
+    }
+
+    private func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(12)
+        }
+    }
+    
+    // MARK: - Public Methods
+
+    func dataBind(text: String) {
+        titleLabel.text = text
+    }
+    
+    func updateTextColor(isSelected: Bool) {
+        titleLabel.textColor = isSelected ? .secondary700 : .neutral700
+    }
+}
