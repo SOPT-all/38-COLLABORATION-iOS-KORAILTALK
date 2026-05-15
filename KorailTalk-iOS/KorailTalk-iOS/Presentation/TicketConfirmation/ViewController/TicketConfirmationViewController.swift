a413cb9d8ae6eede36ad6ec53a2cf8501eff40f4
ser0kim <srkim0728@gmail.com>
2026-05-15 15:11:45 +0900

    [chore] #19 - 네비게이션바, 드롭다운 컴포넌트 Test

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/TicketConfirmation/ViewController/TicketConfirmationViewController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/TicketConfirmation/ViewController/TicketConfirmationViewController.swift
@@ -14,19 +14,6 @@ final class TicketConfirmationViewController: BaseUIViewController {
     
     // MARK: - Property
     
-    private let dropdownView = DropdownView(
-        items: [
-            "일반석",
-            "콘센트석",
-            "유아동반",
-            "수동휠체어",
-            "전동휠체어",
-            "2층석",
-            "자전거",
-            "대피도우미"
-        ],
-        placeholder: "일반석"
-    )
     
     // MARK: - Life Cycle
     
@@ -46,15 +33,10 @@ final class TicketConfirmationViewController: BaseUIViewController {
     }
     
     override func setUI() {
-        view.addSubview(dropdownView)
+        
     }
     
     override func setLayout() {
-        dropdownView.snp.makeConstraints {
-            $0.top.equalTo(navigationBar.snp.bottom).offset(40)
-            $0.leading.equalToSuperview().inset(20)
-            $0.width.equalTo(92)
-            $0.height.equalTo(28)
-        }
+
     }
 }
