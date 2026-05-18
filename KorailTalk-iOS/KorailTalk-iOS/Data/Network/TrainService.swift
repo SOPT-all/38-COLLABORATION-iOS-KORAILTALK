548660ccf195381aa513197115585851be534435
GyeongTaek Kim (mansuiki) <mansuiki@gmail.com>
2026-05-18 23:31:42 +0900

    [feat] #45 - API Entity 추가 및 기본 Network 서비스 연동

--- KorailTalk-iOS/KorailTalk-iOS/Data/Network/TrainService.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Data/Network/TrainService.swift
@@ -13,7 +13,17 @@ final class TrainService: BaseService, TrainServiceProtocol {
     }
     
     func fetchSeats(scheduleId: Int, completion: @escaping (Result<[Seat], any Error>) -> Void) {
-        
+        request(
+            urlString: APIConfiguration.fetchSeats(scheduleId: scheduleId).urlString
+        ) { (result: Result<SeatListEntity, Error>) in
+            switch result {
+            case .success(let entity):
+                completion(.success(entity.toDomain()))
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
     
     func reserveSeats(scheduleId: Int, userId: Int, seatNumbers: [Int], completion: @escaping (Result<Reservation, any Error>) -> Void) {
