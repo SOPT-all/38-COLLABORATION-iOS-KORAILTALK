e2b2abba531d4055f658a1318642d85b713a23ab
jiinjang <jiinjang0@gmail.com>
2026-05-18 23:38:47 +0900

    [chore] #37 - test용 model 수정

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/TrainSearch/Model/TrainModel.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/TrainSearch/Model/TrainModel.swift
@@ -8,20 +8,29 @@
 import UIKit
 
 struct TrainModel {
-    let trainName: String
-    let departureTime: String
-    let arrivalTime: String
-    let durationTime: String
-    let benefitText: String?
-    let standardFare: Int
-    let specialFare: Int
+    let trainInfo: TrainInfo
+    let fareInfo: TrainFare
+    let hasOutletSeat: Bool
     
-    var standardFareString: String {
-        return formatNumber(standardFare)
+    var trainName: String {
+        return trainInfo.name
     }
     
-    var specialFareString: String {
-        return formatNumber(specialFare)
+    var trainType: String {
+        return trainInfo.type.title
+    }
+    
+    var standardFareString: String {
+        return formatNumber(fareInfo.general.price)
+    }
+    
+    var specialFareString: String? {
+        guard let specialPrice = fareInfo.special?.price
+        else {
+            return nil
+        }
+        
+        return formatNumber(specialPrice)
     }
     
     private func formatNumber(_ number: Int) -> String {
@@ -29,17 +38,123 @@ struct TrainModel {
         formatter.numberStyle = .decimal
         return formatter.string(from: NSNumber(value: number)) ?? "\(number)"
     }
+    
+    var departureTime: String {
+        return formatDate(trainInfo.time.departureTime)
+    }
+    
+    var arrivalTime: String {
+        return formatDate(trainInfo.time.arrivalTime)
+    }
+    
+    var durationTime: String {
+        return formatDuration(from: trainInfo.time.departureTime, to: trainInfo.time.arrivalTime)
+    }
+    
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+    
+    private func formatDuration(from departureTime: Date, to arrivalTime: Date) -> String {
+        let totalMinutes = Int(arrivalTime.timeIntervalSince(departureTime) / 60)
+        return "\(totalMinutes / 60)시간 \(totalMinutes % 60)분"
+    }
 }
 
 extension TrainModel {
     static func dummy() -> [TrainModel] {
         return [
-            TrainModel(trainName: "KTX 001", departureTime: "06:00", arrivalTime: "08:35", durationTime: "2시간 35분", benefitText: "5% 적립", standardFare: 59800, specialFare: 83700),
-            TrainModel(trainName: "KTX-산천 402", departureTime: "07:15", arrivalTime: "10:02", durationTime: "2시간 47분", benefitText: "임산부 할인", standardFare: 48200, specialFare: 65000),
-            TrainModel(trainName: "ITX-마음 1101", departureTime: "08:30", arrivalTime: "12:45", durationTime: "4시간 15분", benefitText: nil, standardFare: 34600, specialFare: 52000),
-            TrainModel(trainName: "무궁화호 1205", departureTime: "09:10", arrivalTime: "14:20", durationTime: "5시간 10분", benefitText: "최저가", standardFare: 28600, specialFare: 41000),
-            TrainModel(trainName: "SRT 305", departureTime: "10:40", arrivalTime: "13:12", durationTime: "2시간 32분", benefitText: "쿠폰사용가능", standardFare: 52500, specialFare: 76000),
-            TrainModel(trainName: "KTX 023", departureTime: "11:55", arrivalTime: "14:30", durationTime: "2시간 35분", benefitText: nil, standardFare: 59800, specialFare: 83700)
+            TrainModel(
+                trainInfo: TrainInfo(
+                    type: .ktx,
+                    name: "KTX 001",
+                    time: TrainTime(
+                        departureTime: makeDate(hour: 6, minute: 0),
+                        arrivalTime: makeDate(hour: 8, minute: 35)
+                    ),
+                    availability: TrainAvailability(isSoldOut: false, isOutletSoldOut: false)
+                ),
+                fareInfo: TrainFare(
+                    general: SeatFare(type: .general, price: 59_800, isAvailable: true),
+                    special: SeatFare(type: .special, price: 83_700, isAvailable: true)
+                ),
+                hasOutletSeat: true
+            ),
+            TrainModel(
+                trainInfo: TrainInfo(
+                    type: .ktxSancheon,
+                    name: "KTX-산천 402",
+                    time: TrainTime(
+                        departureTime: makeDate(hour: 7, minute: 15),
+                        arrivalTime: makeDate(hour: 10, minute: 2)
+                    ),
+                    availability: TrainAvailability(isSoldOut: false, isOutletSoldOut: true)
+                ),
+                fareInfo: TrainFare(
+                    general: SeatFare(type: .general, price: 48_200, isAvailable: true),
+                    special: SeatFare(type: .special, price: 65_000, isAvailable: true)
+                ),
+                hasOutletSeat: true
+            ),
+            TrainModel(
+                trainInfo: TrainInfo(
+                    type: .itxSaemaeul,
+                    name: "ITX-새마을 1101",
+                    time: TrainTime(
+                        departureTime: makeDate(hour: 8, minute: 30),
+                        arrivalTime: makeDate(hour: 12, minute: 45)
+                    ),
+                    availability: TrainAvailability(isSoldOut: false, isOutletSoldOut: false)
+                ),
+                fareInfo: TrainFare(
+                    general: SeatFare(type: .general, price: 34_600, isAvailable: true),
+                    special: nil
+                ),
+                hasOutletSeat: false
+            ),
+            TrainModel(
+                trainInfo: TrainInfo(
+                    type: .mugunghwa,
+                    name: "무궁화호 1205",
+                    time: TrainTime(
+                        departureTime: makeDate(hour: 9, minute: 10),
+                        arrivalTime: makeDate(hour: 14, minute: 20)
+                    ),
+                    availability: TrainAvailability(isSoldOut: false, isOutletSoldOut: false)
+                ),
+                fareInfo: TrainFare(
+                    general: SeatFare(type: .general, price: 28_600, isAvailable: true),
+                    special: nil
+                ),
+                hasOutletSeat: false
+            ),
+            TrainModel(
+                trainInfo: TrainInfo(
+                    type: .ktx,
+                    name: "KTX 023",
+                    time: TrainTime(
+                        departureTime: makeDate(hour: 11, minute: 55),
+                        arrivalTime: makeDate(hour: 14, minute: 30)
+                    ),
+                    availability: TrainAvailability(isSoldOut: false, isOutletSoldOut: false)
+                ),
+                fareInfo: TrainFare(
+                    general: SeatFare(type: .general, price: 59_800, isAvailable: true),
+                    special: SeatFare(type: .special, price: 83_700, isAvailable: true)
+                ),
+                hasOutletSeat: true
+            )
         ]
     }
+    
+    private static func makeDate(hour: Int, minute: Int) -> Date {
+        var components = Calendar.current.dateComponents([.year, .month, .day], from: Date())
+        components.hour = hour
+        components.minute = minute
+        
+        return Calendar.current.date(from: components) ?? Date()
+    }
 }
+
