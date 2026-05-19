a4949b11c5deade33dab4b8c921284bc6cf3fc11
sumchive <haon0319@naver.com>
2026-05-19 11:48:57 +0900

    [feat] #43 - MyTicketCollectionViewController 구현

--- KorailTalk-iOS/KorailTalk-iOS/Presentation/MyTicket/ViewController/MyTicketViewController.swift
+++ KorailTalk-iOS/KorailTalk-iOS/Presentation/MyTicket/ViewController/MyTicketViewController.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
-class MyTicketViewController: BaseUIViewController {
+import SnapKit
+import Then
+
+final class MyTicketViewController: BaseUIViewController {
     
+    // MARK: - UI Components
+    
+    private let myTicketCollectionView = MyTicketCollectionView()
+    
+    // MARK: - Custom Methods
+    
+    override func setStyle() {
+        navigationBar.configure(title: "나의 티켓", showsRefreshButton: false)
+        view.backgroundColor = .neutral100
+    }
+    
+    override func setUI() {
+        view.addSubview(myTicketCollectionView)
+    }
+    
+    override func setLayout() {
+        myTicketCollectionView.snp.makeConstraints {
+            $0.top.equalTo(navigationBar.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(5)
+        }
+    }
 }
